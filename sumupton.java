03286678a199da266178450da4a8c027135af400
Khush <98575606+Khush-S@users.noreply.github.com>
2023-04-01 00:12:57 +0530

    Add files via upload

---
+++ sumupton.java
@@ -0,0 +1,15 @@
+ Scanner(System.in);
+    n=sc.nextint();
+    for(int i=0,i<=n,i++);
+    {package lang;
+import java.util.scanner;
+public class sumupton{
+  public static void main(Strings[]args){
+    int n,sum=0;
+    Scanner sc=new
+      sum+=i;
+    }
+    system.out.println(sum);
+    
+  }
+}
